c4b756af6ca5175b9c4aa1be2184c42203b05b77
Amsavarthan Lv <amsavarthan.a@gmail.com>
2020-04-10 02:51:26 +0530

    Updates

---
+++ precache-manifest.c13706f523eaa68a9606e747aa68bb5f.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534a50b359e3b4b624d740d31a6e4f12",
+    "url": "/lvstore/index.html"
+  },
+  {
+    "revision": "5fba6f7099cc3ee9da53",
+    "url": "/lvstore/static/css/2.8c7e31f6.chunk.css"
+  },
+  {
+    "revision": "a90d6f62e15cd0e37a99",
+    "url": "/lvstore/static/css/main.880e8bb2.chunk.css"
+  },
+  {
+    "revision": "5fba6f7099cc3ee9da53",
+    "url": "/lvstore/static/js/2.3edccfb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lvstore/static/js/2.3edccfb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90d6f62e15cd0e37a99",
+    "url": "/lvstore/static/js/main.5c0e5011.chunk.js"
+  },
+  {
+    "revision": "ea5b2e618b3a9d8d1d65",
+    "url": "/lvstore/static/js/runtime-main.d5532834.js"
+  },
+  {
+    "revision": "6fd43fafe189ba25aebf639c9e2d41da",
+    "url": "/lvstore/static/media/Flames.6fd43faf.png"
+  },
+  {
+    "revision": "f6c17dc2ca6a6f52605361b86bcd0608",
+    "url": "/lvstore/static/media/Hify.f6c17dc2.png"
+  },
+  {
+    "revision": "7658ffd088bbfb89959fea7ac7a11c26",
+    "url": "/lvstore/static/media/Media Toolbox.7658ffd0.png"
+  },
+  {
+    "revision": "c636a04f35a81025a1429b62466770ba",
+    "url": "/lvstore/static/media/Paruvaththe Payir Sei.c636a04f.png"
+  },
+  {
+    "revision": "9ed9a4135b0454f4d12d9a6f523a6c6f",
+    "url": "/lvstore/static/media/Posizione.9ed9a413.png"
+  },
+  {
+    "revision": "530a243ca6c20b2a4743ee297072edc3",
+    "url": "/lvstore/static/media/Trivia.530a243c.png"
+  },
+  {
+    "revision": "afb15ada8caec924230e90fe65337a67",
+    "url": "/lvstore/static/media/developer.afb15ada.webp"
+  },
+  {
+    "revision": "62a1b5828c143b96946b166d17e4488c",
+    "url": "/lvstore/static/media/js.62a1b582.png"
+  },
+  {
+    "revision": "ba4fe7383b8d9f538dacbb7c1dcf2d54",
+    "url": "/lvstore/static/media/python.ba4fe738.png"
+  },
+  {
+    "revision": "4ade5cac5aba1b2a6129e12ce9a93deb",
+    "url": "/lvstore/static/media/reactlogo.4ade5cac.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/lvstore/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/lvstore/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/lvstore/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/lvstore/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/lvstore/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/lvstore/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/lvstore/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/lvstore/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/lvstore/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/lvstore/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/lvstore/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/lvstore/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/lvstore/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/lvstore/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/lvstore/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/lvstore/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/lvstore/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/lvstore/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/lvstore/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/lvstore/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/lvstore/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/lvstore/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/lvstore/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/lvstore/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  }
+]);
